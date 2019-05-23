ec45e0c2e26fdd70ab3ef22763fe3a6f2e449952
Matt Williams <matt@williams-tech.net>
2019-05-23 13:37:14 -0400

    Added ES/Kibana support and Keystore

--- build.sh
+++ build.sh
@@ -7,11 +7,12 @@ SID=$(uuid)
 #TODO - enforce params
 namespace=$1
 env=$2
+action=$3
 
 rm -f configs/*.yml
 
 $AWS lambda list-functions | awk "/FunctionName/ && /${namespace}/ { print \$2 }" \
-  | egrep -v 'logger|datadog|testola|thumb|jasons|matts' \
+  | egrep -v 'logger|datadog|testola|thumb|jasons' \
   | sed 's/"//g;s/,//' | sort >|log_groups.txt
 
 split=30
@@ -21,7 +22,7 @@ entry=""
 
 for fn in `cat log_groups.txt`; do
 
-  entry="      - log_group_name: /aws/lambda/${fn}"
+  entry="      - { log_group_name: /aws/lambda/${fn}, filter_pattern: INFO }"
 
   if [[ $((iter % split)) -eq 0 ]]; then
 
@@ -45,6 +46,6 @@ rm -f log_groups.txt
 set -x
 iter=0
 for conf in `ls configs`; do
-  ./functionbeat deploy -c functionbeat.yml -c configs/${conf} -e -d "*" ${namespace}-log-processor${iter}
+  ./functionbeat $action -c functionbeat.yml -c configs/${conf} -e -d "*" ${namespace}-log-processor${iter}
   iter=$((iter + 1))
 done
